dd4cf25d23c41620b74830436e951fc92abc7eda
Miriam Poletti <polettimiriam@gmail.com>
2024-11-15 23:19:02 +0100

    styling homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,28 @@
 import './Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTiktok, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
+
 
 const Home = () => {
     return <div className='header'>
-        <header>
-            <h1>My name is Billa</h1>
-            <p>I'm a designer from Gaza</p>
-        </header>
-        <div className='social-media-icons'>
+        <header className='header-text'>
+            <h1 className='header-text-bold'>My name is Billa</h1>
+            <p className='header-text-light'>I'm a designer from Gaza</p>
+            <div className='social-media-icons'>
             <a href="https://www.instagram.com/eizaabilla/" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faInstagram} />
+                <FontAwesomeIcon icon={faInstagram} className='social-media-icon' />
             </a>
             <a href="https://www.tiktok.com/@_isaabilla_?_t=8qmMlKPznl3&_r=1" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faTiktok} />
+            <FontAwesomeIcon icon={faTiktok} className='social-media-icon' />
             </a>
             <a href="https://www.x.com/eizaabilla/" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faXTwitter} />
+            <FontAwesomeIcon icon={faXTwitter} className='social-media-icon' />
             </a>
         </div>
 
+        <FontAwesomeIcon icon={faArrowDownLong} className='arrow-down' />
+        </header>  
     </div>
 }
 
