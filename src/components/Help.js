319bf6ca72c81259d19395fc400eea032f38be40
Miriam Poletti <polettimiriam@gmail.com>
2024-11-19 21:42:44 +0100

    defining design

--- src/components/Help.js
+++ src/components/Help.js
@@ -3,26 +3,28 @@ import HelpPhoto from '../images/photo-help.jpg'
 
 const Help = () => {
     return <div className='help'>
-        <h1 className='section-title'>Why we need help</h1>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-            diam nonumy eirmod tempor invidunt ut labore et dolore
-            magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd
-            gubergren, no sea takimata sanctus est Lorem ipsum dolor
-            sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-            elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos
-            et accusam et justo duo dolores et ea rebum. Stet clita
-            kasd gubergren, no sea takimata sanctus est Lorem ipsum
-            dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-            labore et dolore magna aliquyam erat, sed diam voluptua. At
-            vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem
-            ipsum dolor sit amet.</p>
-            <a href="https://www.gofundme.com/f/aiutateci-ad-evacuare-questa-famiglia-da-gaza" target="_blank" rel="noreferrer">
-                <button>Make a donation</button>
-            </a>
+        <div className='help-text'>
+            <h1 className='section-title'>Why we need help</h1>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et
+                accusam et justo duo dolores et ea rebum. Stet clita kasd
+                gubergren, no sea takimata sanctus est Lorem ipsum dolor
+                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+                elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat, sed diam voluptua. At vero eos
+                et accusam et justo duo dolores et ea rebum. Stet clita
+                kasd gubergren, no sea takimata sanctus est Lorem ipsum
+                dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem
+                ipsum dolor sit amet.</p>
+                <a href="https://www.gofundme.com/f/aiutateci-ad-evacuare-questa-famiglia-da-gaza" target="_blank" rel="noreferrer">
+                    <button>Make a donation</button>
+                </a>   
+        </div>
             <div className='help-img'>
                     <img src={HelpPhoto} alt='help' className='help-photo'/>
             </div>
--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -10,7 +10,7 @@ const Thanks = () => {
                 <img src={ThanksPhoto} alt='thank you' className='thanks-photo'/>
             </div>
 
-            <div>
+            <div className='thanks-text'>
                 <h1 className='section-title'>Thank you!</h1>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore
@@ -20,13 +20,13 @@ const Thanks = () => {
                 sit amet.</p>
                 <p>Don't forget to follow me for updates</p>
                 <div className='social-media-icons'>
-                    <a href="https://www.instagram.com/eizaabilla/" target="_blank" rel="noreferrer">
+                    <a href="https://www.instagram.com/eizaabilla/" target="_blank" rel="noreferrer" className='social-media-icon'>
                         <FontAwesomeIcon icon={faInstagram} />
                     </a>
-                    <a href="https://www.tiktok.com/@_isaabilla_?_t=8qmMlKPznl3&_r=1" target="_blank" rel="noreferrer">
+                    <a href="https://www.tiktok.com/@_isaabilla_?_t=8qmMlKPznl3&_r=1" target="_blank" rel="noreferrer" className='social-media-icon'>
                     <FontAwesomeIcon icon={faTiktok} />
                     </a>
-                    <a href="https://www.x.com/eizaabilla/" target="_blank" rel="noreferrer">
+                    <a href="https://www.x.com/eizaabilla/" target="_blank" rel="noreferrer" className='social-media-icon'>
                     <FontAwesomeIcon icon={faXTwitter} />
                 </a>
             </div>
